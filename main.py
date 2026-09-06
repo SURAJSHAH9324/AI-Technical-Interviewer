08132b8ac567e95f93228005826d319d051f37cc
SURAJSHAH9324 <surajshah77881@gmail.com>
2026-09-06 15:29:18 +0530

    helath api

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from ollama import chat
+#from ollama import chat
+# response = chat(
+#     model="qwen3:8b",
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "What is Python? Answer in 2 short sentences."
+#         }
+#     ]
+# )
 
-response = chat(
-    model="qwen3:8b",
-    messages=[
-        {
-            "role": "user",
-            "content": "What is Python? Answer in 2 short sentences."
-        }
-    ]
-)
+# print(response.message.content)
 
-print(response.message.content)
+from fastapi import FastAPI
+from app.routes.home import router as home_router
+from app.routes.health import router as health_router
+app = FastAPI(title="AI Technical Interviewer")
+
+app.include_router(home_router)
+app.include_router(health_router)
